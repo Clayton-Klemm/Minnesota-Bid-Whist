2bce6e502a6326554b8477657a4abe93ef3f7686
Clayton-Klemm <36232582+Clayton-Klemm@users.noreply.github.com>
2024-09-12 22:03:55 -0500

    testing update

--- main.py
+++ main.py
@@ -6,16 +6,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Minnesota Whist")
-    
     start_screen = StartScreen(screen)
     game = Game(screen)
-    
     # Show the start screen
     start_screen.run()
-    
     # If the start screen exits, start the main game
     game.run()
-    
     pygame.quit()
 
 if __name__ == "__main__":
